e458bf4db32473bc7b93226a174fa7643b2f0547
hyexnn <hyeonzz2795@gmail.com>
2024-06-03 23:39:29 +0900

    메인화면 UI 제작 (미완)

---
+++ Team Project/Assets/Script/MainMenu.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum BTNType
+{
+    New,
+    Continue,
+    Opion,
+    Sound,
+    Back,
+    Quit
+}
+
+public class MainMenu : MonoBehaviour
+{
+  
+}
